4839640f32f3fd540d702a68bdd389697d341b63
markcrowley1 <markcrowley1999@gmail.com>
2022-10-15 02:28:13 +0100

    Added template files for basic tasks

---
+++ run.py
@@ -0,0 +1,4 @@
+"""
+Description:
+    Script for running text contradiction analysis
+"""
---
+++ text-contradiction.py
@@ -0,0 +1,8 @@
+"""
+Description:
+    Class to provide interface with other tools.
+"""
+
+class TextContradiction:
+    def __init__(self):
+        pass
