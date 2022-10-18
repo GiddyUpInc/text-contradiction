ee6d69dffa4a3aa661462a552aede83faa841306
cormacmadden <comadden@Gmail.com>
2022-10-18 14:48:28 +0100

    Added tfds.

--- train.py
+++ train.py
@@ -9,21 +9,14 @@ import tensorflow_datasets.public_api as tfds
 import pandas as pd
 
 def define_and_parse_args():
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("-i", "--input", type=str, required=True, help="Input name of single txt or json file for training.")
-    
-    args = parser.parse_args()
-    
-    output = {
-        "input": args.input
-    }
-    
-    return output
+    pass
 
-def load_data(filename: str):
-    json_obj = pd.read_json(path_or_buf=filename, lines=True)
-    return json_obj
+def load_data(dataset: str):
+    """Load data from tensor flow dataset api"""
+    tfds.load(
+        name = dataset,
+        data_dir="./data"
+    )
 
 def create_model():
     pass
@@ -32,12 +25,10 @@ def train_model():
     pass
 
 def main():
-    # Get user args
-    args = define_and_parse_args()
-    filename = args["input"]
-    
     # Load data
-    data = load_data(filename)
+    dataset = "snli"
+    data = load_data(dataset)
+    print(type(data))
     
     # Create model
     model = create_model()
